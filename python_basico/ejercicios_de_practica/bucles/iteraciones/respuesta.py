ec216fc2fd5c96580da3a44f3a9dd2b2ab69616f
Alfonso Torres <alfonsotorres669@gmail.com>
2026-07-08 10:25:33 -0400

    hecho

---
+++ python_basico/ejercicios_de_practica/bucles/iteraciones/respuesta.py
@@ -0,0 +1,39 @@
+
+
+
+
+
+"""
+## Problema 6: Contar números pares
+
+Recorre una lista de números e indica cuántos de ellos son pares.
+
+---
+
+## Problema 7: Mostrar una tupla
+
+Dada una tupla con los meses del año, muestra cada mes en una línea distinta.
+
+---
+
+## Problema 8: Recorrer un conjunto
+
+Dado un conjunto de colores, muestra cada color en pantalla.
+
+---
+
+## Problema 9: Mostrar claves y valores de un diccionario
+
+Dado un diccionario con información de un estudiante (nombre, edad y carrera), recorre el diccionario mostrando cada clave junto con su valor correspondiente.
+
+---
+
+## Problema 10: Contar una letra específica
+
+Recorre una palabra e indica cuántas veces aparece una letra ingresada por el usuario.
+
+---
+
+## Problema 11: Buscar un elemento en una lista
+
+Recorre una lista de frutas y determina si existe una fruta indicada por el usuario. Si la encuentras, muestra un mensaje indicando que fue encontrada; de lo contrario, informa que no está en la lista."""
